48bec1da92fbd8fe66c2af0576d43a07d3f44498
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2025-05-16 22:43:05 +0900

    feat: 마이페이지 조회 로직 구현

--- src/main/java/com/capstone/jejutoon/member/controller/MemberController.java
+++ src/main/java/com/capstone/jejutoon/member/controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.capstone.jejutoon.member.controller;
 
+import com.capstone.jejutoon.member.dto.response.MyPageDto;
+import com.capstone.jejutoon.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/me")
+    ResponseEntity<MyPageDto> getMyPage() {
+        MyPageDto response = memberService.getMyPage();
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/capstone/jejutoon/member/service/MemberService.java
+++ src/main/java/com/capstone/jejutoon/member/service/MemberService.java
@@ -1,4 +1,8 @@
 package com.capstone.jejutoon.member.service;
 
+import com.capstone.jejutoon.member.dto.response.MyPageDto;
+
 public interface MemberService {
+
+    MyPageDto getMyPage();
 }
